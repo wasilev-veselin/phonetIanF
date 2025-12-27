2d285c6aa3ae82d6f5e5d711cb3db7497014bef6
echo <dm1@mail.bg>
2025-12-27 12:20:09 +0200

    style cards

--- src/app/pages/book-detail/book-detail.component.ts
+++ src/app/pages/book-detail/book-detail.component.ts
@@ -1,20 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Observable, map, switchMap } from 'rxjs';
+
+import { Book } from '../../../core/model/book.model';
+import { BooksService } from '../../../core/services/books.service';
 
 @Component({
   selector: 'app-book-detail',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './book-detail.component.html',
-  styleUrl: './book-detail.component.scss'
+  styleUrl: './book-detail.component.scss',
 })
 export class BookDetailComponent implements OnInit {
-
+  private readonly booksService = inject(BooksService);
+  private readonly route = inject(ActivatedRoute);
+  selectedBook$?: Observable<Book>;
 
   ngOnInit(): void {
-
+    this.selectedBook$ = this.route.paramMap.pipe(
+      map((params) => params.get('id') ?? ''),
+      switchMap((id) => this.booksService.getBook(id))
+    );
   }
-
-
 }
--- src/app/pages/book-list/book-list.component.ts
+++ src/app/pages/book-list/book-list.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Book } from '../../../core/model/book.model';
 import { BooksService } from '../../../core/services/books.service';
@@ -9,19 +9,21 @@ import { BooksService } from '../../../core/services/books.service';
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, DatePipe],
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss', '../../../core/styles/page.scss'],
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent {
   private readonly booksService = inject(BooksService);
 
-  readonly books$: Observable<Book[]> = this.booksService.getBooks().pipe(
-    tap((books) => console.log('getBooks()', books))
-  );
+  readonly books$: Observable<Book[]> = this.booksService.getBooks();
   selectedBook$?: Observable<Book>;
 
   selectBook(id: string): void {
     this.selectedBook$ = this.booksService.getBook(id);
+  }
+
+  toggleFavorite(book: Book): void {
+    console.log('toggleFavorite',book)
   }
 }
